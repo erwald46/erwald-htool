8474cc494c6000d5bbefa43ac4b6ecc6ee169885
BerkTruy <82322438+BerkTruy@users.noreply.github.com>
2021-05-08 03:20:08 +0300

    Create Prang.py

---
+++ Prang.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+
+
+
+
+import os
+
+os.system("apt install cowsay")
+
+os.system("pkg install toilet")
+
+os.system("clear")
+
+os.system("toilet -f mono12 -F metal prang")
+print("""
+
+Coder by Zone
+
+
+[1] Impulse              [11] Instagram-Tools
+
+[2] Shrzon               [12] Phone Infoga
+
+[3] SZ-PORT-SCAN         [13] Eagle
+
+[4] Games                [14] Cewl
+
+[5] Turk-Sploit          [15] A-Rat
+
+[6] Camera-T             [16] Atscanr
+
+[7] TC-Kimlik            [17] Baf
+
+[8] Grabcam              [18] Badmod
+
+[9] Luzymux              [19] Devploit
+
+[10] Shark               [20] Auxsacn
+
+
+
+
+
+
+""")
+
+
+
+
+
+giris = input("----->>>")
+
+
+
+
+if giris=="1":
+
+
+     os.system("git clone https://github.com/LimerBoy/Impulse")
+     os.system("clear")
+     os.system("cowsaay KurulumBitti ")
+
+if giris=="2":
+
+     os.system("git clone https://github.com/shriyusan/Shrzon")
+     os.system("clear")
+     os.system("cowsay KurulumBitti ")
+
+if giris=="3":
+
+     os.system("git clone https://github.com/shriyusan/SZ-PORT-SCAN")
+     os.system("clear")
+     os.system("cowsay KurulumBitti ")
+
+if giris=="4":
+
+     os.system("git clone https://github.com/shriyusan/Games")
+     os.system("clear")
+     os.system("cowsay KurulumBitti ")
+
+if giris=="5":
+
+     os.system("git clone https://github.com/yamanefkar/Turk-Sploit")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="6":
+
+     os.system("git clone https://github.com/yamanefkar/Camera-Tr")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="7":
+
+     os.system("git clone https://github.com/yamanefkar/Kimlik-Dogrulama")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+
+if giris=="8":
+
+     os.system("git clone https://github.com/noob-hackers/grabcam")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="9":
+
+     os.system("git clone https://github.com/Gameye98/Lazymux")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+
+if giris=="10":
+
+     os.system("git clone https://github.com/Bhaviktutorials/shark")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+
+if giris=="11":
+
+     os.system("git clone https://github.com/masokky/instagram-tools")
+     os.system("clear")
+     os.systen("cowsay KurulumBitti")
+
+
+
+if giris=="12":
+
+     os.system("git clone https://github.com/sundowndev/phoneinfoga")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="13":
+
+     os.system("git clone https://github.com/ThoughtfulDev/EagleEye")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="14":
+
+     os.system("git clone https://github.com/digininja/CeWL")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="15":
+
+     os.system("git clone https://github.com/AhMyth/AhMyth-Android-RAT")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="16":
+
+     os.system("git clone https://github.com/AlisamTechnology/ATSCAN")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="17":
+
+     os.system("git clone https://github.com/engMaher/BAF")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="18":
+
+    os.system("git clone https://github.com/M4DM0e/BadMod")
+    os.system("clear")
+    os.system("cowsay KurulumBitti")
+
+if giris=="19":
+
+     os.system("git clone https://github.com/GhettoCole/Devploit")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
+
+if giris=="20":
+
+     os.system("git clone https://github.com/Gameye98/Auxscan")
+     os.system("clear")
+     os.system("cowsay KurulumBitti")
